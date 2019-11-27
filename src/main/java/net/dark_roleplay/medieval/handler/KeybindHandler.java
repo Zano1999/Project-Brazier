8772858662e0207c61b4cf0b9609ef11558e0d32
JTK222 <jtk222.lukas@gmail.com>
2019-11-28 00:50:59 +0100

    Dashing!
    Added a dash ability, which is ment to be used for dodging projectiles.

--- src/main/java/net/dark_roleplay/medieval/handler/KeybindHandler.java
+++ src/main/java/net/dark_roleplay/medieval/handler/KeybindHandler.java
@@ -1,16 +1,13 @@
 package net.dark_roleplay.medieval.handler;
 
 import net.dark_roleplay.medieval.DarkRoleplayMedieval;
-import net.dark_roleplay.crafter.objects.guis.CraftingScreen;
+import net.dark_roleplay.medieval.holders.MedievalConfigs;
 import net.dark_roleplay.medieval.objects.blocks.decoration.road_sign.RoadSignHelper;
-import net.dark_roleplay.medieval.objects.guis.fourteen.TimberedArea;
-import net.dark_roleplay.medieval.objects.guis.fourteen.TimberedScreen;
-import net.dark_roleplay.medieval.testing.Keybinds;
+import net.dark_roleplay.medieval.objects.packets.DodgePacket;
 import net.minecraft.client.GameSettings;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
@@ -31,6 +28,10 @@ public class KeybindHandler {
             //Minecraft.getInstance().displayGuiScreen(new TimberedScreen(new TimberedArea(Minecraft.getInstance().world, new BlockPos(-1031, 70, 821), new BlockPos(-1031, 72, 815))));
             RoadSignHelper.INSTANCE.setRight(!RoadSignHelper.INSTANCE.isRight());
         }else if(DODGE.isPressed()){
+            PlayerEntity player = Minecraft.getInstance().player;
+
+            if(!MedievalConfigs.SKILL_CONFIG.ALLOOW_AIRBORNE.get() && !player.onGround) return;
+
             GameSettings binds = Minecraft.getInstance().gameSettings;
 
             boolean f = binds.keyBindForward.isKeyDown(), b = binds.keyBindBack.isKeyDown(), r = binds.keyBindRight.isKeyDown(), l = binds.keyBindLeft.isKeyDown();
@@ -47,16 +48,16 @@ public class KeybindHandler {
                }
             }
 
-            PlayerEntity p = Minecraft.getInstance().player;
-            double radianYaw = (offset + p.getRotationYawHead()) * (Math.PI / 180);
+            double radianDirection = (offset + player.getRotationYawHead()) * (Math.PI / 180);
 
-            Vec3d dodgeVec = new Vec3d(-Math.sin(radianYaw), 0, Math.cos(radianYaw)).normalize();
+            DarkRoleplayMedieval.MOD_CHANNEL.sendToServer(new DodgePacket((float) radianDirection));
 
-            Vec3d motion = p.getMotion();
-            double speedMult = p.collidedVertically ? p.isSprinting() ? 0.3 : 0.5 : p.isSprinting() ? 0.05 : 0.3;
-            System.out.println(p.collidedVertically);
+            Vec3d dodgeVec = new Vec3d(-Math.sin(radianDirection), 0, Math.cos(radianDirection)).normalize();
 
-            p.setVelocity(motion.x + dodgeVec.x * speedMult, 0.3, motion.z + dodgeVec.z * speedMult);
+            Vec3d motion = player.getMotion();
+            double speedMult = player.collidedVertically ? player.isSprinting() ? 0.3 : 0.5 : player.isSprinting() ? 0.05 : 0.3;
+
+            player.setVelocity(motion.x + dodgeVec.x * speedMult, 0.3, motion.z + dodgeVec.z * speedMult);
             //TODO Send packet to server, remove fall damage add config for this.
         }
     }
