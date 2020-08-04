12967a5fce7742a449adf7447c6ef21e9a6e8683
JTK222 <lukasporwit3@googlemail.com>
2020-08-04 16:14:39 +0200

    Fixed mixin issue

--- src/main/java/net/dark_roleplay/projectbrazier/mixin/CollisionMixin.java
+++ src/main/java/net/dark_roleplay/projectbrazier/mixin/CollisionMixin.java
@@ -10,10 +10,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Entity.class)
 public class CollisionMixin {
 
-    @Inject(method="getAllowedMovement(Lnet/minecraft/util/math/vector/Vector3d;)Lnet/minecraft/util/math/vector/Vector3d", at=@At("TAIL"), cancellable = true)
+    @Inject(method="getAllowedMovement(Lnet/minecraft/util/math/vector/Vector3d;)Lnet/minecraft/util/math/vector/Vector3d;", at=@At("TAIL"), cancellable = true)
     public void considerAdditionalCollision(Vector3d vec, CallbackInfoReturnable<Vector3d> info) {
-        System.out.println(vec);
+        //System.out.println(vec);
 
-        info.setReturnValue(vec);
+        info.setReturnValue(info.getReturnValue());
     }
 }
