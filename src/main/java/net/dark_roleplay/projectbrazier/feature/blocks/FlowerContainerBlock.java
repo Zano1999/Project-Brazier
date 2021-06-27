3737bf336f47e62309fd50a304e0e8d63fe15789
JTK222 <jtk222.lukas@gmail.com>
2021-06-27 17:27:02 +0200

    Allowed to put non FlowerBlocks into Flower Buckets
    Add items into the tag "projectbrazier:pot_plant" to allow putting them in.

--- src/main/java/net/dark_roleplay/projectbrazier/feature/blocks/FlowerContainerBlock.java
+++ src/main/java/net/dark_roleplay/projectbrazier/feature/blocks/FlowerContainerBlock.java
@@ -1,5 +1,6 @@
 package net.dark_roleplay.projectbrazier.feature.blocks;
 
+import net.dark_roleplay.projectbrazier.ProjectBrazier;
 import net.dark_roleplay.projectbrazier.feature.blockentities.FlowerContainerBlockEntity;
 import net.dark_roleplay.projectbrazier.feature.blocks.templates.DecoBlock;
 import net.dark_roleplay.projectbrazier.util.capabilities.ItemHandlerUtil;
@@ -7,11 +8,14 @@ import net.dark_roleplay.projectbrazier.util.json.VoxelShapeLoader;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
@@ -20,10 +24,13 @@ import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.math.vector.Vector3i;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeTagHandler;
 import net.minecraftforge.common.Tags;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class FlowerContainerBlock extends DecoBlock {
 
+	private static Tags.IOptionalNamedTag<Item> POT_PLANTS = ForgeTagHandler.createOptionalTag(ForgeRegistries.ITEMS, new ResourceLocation(ProjectBrazier.MODID, "pot_plant"));
 	private VoxelShape allowedPlacementArea;
 
 	public FlowerContainerBlock(Properties properties, String shapeName, String allowedPlacement) {
@@ -45,7 +52,7 @@ public class FlowerContainerBlock extends DecoBlock {
 			if(!stack.isEmpty())
 				player.addItemStackToInventory(stack);
 		}else {
-			if(ItemTags.FLOWERS.contains(heldItem.getItem())) {
+			if(heldItem.getItem() instanceof BlockItem && (ItemTags.FLOWERS.contains(heldItem.getItem()) || POT_PLANTS.contains(heldItem.getItem()))) {
 				Vector3d hitPos = hit.getHitVec();
 				Vector3i offsetPos = new Vector3i((int) Math.floor((hitPos.getX() - pos.getX()) * 16), (int) Math.floor((hitPos.getY() - pos.getY()) * 16), (int) Math.floor((hitPos.getZ() - pos.getZ()) * 16));
 
