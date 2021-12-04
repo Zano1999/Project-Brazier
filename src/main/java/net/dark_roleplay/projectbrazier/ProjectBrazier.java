accaba506a03e551b6b6160fb280b8e8af1aea37
JTK222 <jtk222.lukas@gmail.com>
2021-12-04 22:59:53 +0100

    Started 1.18 port

--- src/main/java/net/dark_roleplay/projectbrazier/ProjectBrazier.java
+++ src/main/java/net/dark_roleplay/projectbrazier/ProjectBrazier.java
@@ -1,17 +1,13 @@
 package net.dark_roleplay.projectbrazier;
 
-import net.dark_roleplay.projectbrazier.feature.blockentities.HangingItemBlockEntity;
-import net.dark_roleplay.projectbrazier.feature.registrars.*;
 import net.dark_roleplay.projectbrazier.experimental_features.decorator.DecorRegistrar;
-import net.minecraft.block.Blocks;
-import net.minecraft.item.Item;
-import net.minecraft.tileentity.TileEntityType;
+import net.dark_roleplay.projectbrazier.feature.registrars.*;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
@@ -20,57 +16,59 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(ProjectBrazier.MODID)
 public class ProjectBrazier {
 
-    public static final String MODID = "projectbrazier";
+	public static final String MODID = "projectbrazier";
 
-    public ProjectBrazier() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+	public ProjectBrazier() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        BrazierPackets.registerPackets();
+		BrazierPackets.registerPackets();
 
-        MinecraftForge.EVENT_BUS.addListener(BrazierCommands::registerCommands);
+		MinecraftForge.EVENT_BUS.addListener(BrazierCommands::registerCommands);
 
-        BrazierRegistries.BLOCKS.register(modEventBus);
-        BrazierRegistries.BLOCKS_NO_ITEMS.register(modEventBus);
-        BrazierRegistries.BLOCK_ENTITIES.register(modEventBus);
-        BrazierRegistries.ITEMS.register(modEventBus);
-        BrazierRegistries.CONTAINERS.register(modEventBus);
-        BrazierRegistries.ENTITIES.register(modEventBus);
-        BrazierRegistries.SOUNDS.register(modEventBus);
+		BrazierRegistries.BLOCKS.register(modEventBus);
+		BrazierRegistries.BLOCKS_NO_ITEMS.register(modEventBus);
+		BrazierRegistries.BLOCK_ENTITIES.register(modEventBus);
+		BrazierRegistries.ITEMS.register(modEventBus);
+		BrazierRegistries.CONTAINERS.register(modEventBus);
+		BrazierRegistries.ENTITIES.register(modEventBus);
+		BrazierRegistries.SOUNDS.register(modEventBus);
 
-        modEventBus.addListener(this::hackyHackToByPassLoadingOrder);
-        modEventBus.addListener(this::setupCommonStuff);
-        modEventBus.addListener(this::setupServerStuff);
+		modEventBus.addListener(this::hackyHackToByPassLoadingOrder);
+		modEventBus.addListener(this::setupCommonStuff);
+		modEventBus.addListener(this::setupServerStuff);
 
-        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ProjectBrazierClient::modConstructor);
+		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ProjectBrazierClient::modConstructor);
 
 
-        DecorRegistrar.register();
-        //Decals
+		DecorRegistrar.register();
+		//Decals
 //        FMLJavaModLoadingContext.get().getModEventBus().addListener(DecalRegistry::registerRegistry);
 //        MinecraftForge.EVENT_BUS.addGenericListener(Chunk.class, CapabilityAttachListener::attachChunkCapability);
 //        CapabilityManager.INSTANCE.register(DecalChunk.class, new DecalChunkStorage(), () -> new DecalChunk(0, 256)); //TODO 1.17 update the default to new default heights.
-    }
+	}
 
-    public void hackyHackToByPassLoadingOrder(RegistryEvent.NewRegistry event){
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+	public void hackyHackToByPassLoadingOrder(RegistryEvent.NewRegistry event) {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        BrazierBlocks.preRegistry();
-        BrazierBlockEntities.preRegistry();
-        BrazierItems.preRegistry();
-        BrazierContainers.preRegistry();
-        BrazierEntities.preRegistry();
-        BrazierSounds.preRegistry();
+		BrazierBlocks.preRegistry();
+		BrazierBlockEntities.preRegistry();
+		BrazierItems.preRegistry();
+		BrazierContainers.preRegistry();
+		BrazierEntities.preRegistry();
+		BrazierSounds.preRegistry();
 
-        modEventBus.addListener(BrazierBlocks::postRegistry);
-        modEventBus.addListener(BrazierBlockEntities::postRegistry);
-        modEventBus.addGenericListener(Item.class, BrazierItems::registerItemBlocks);
-        modEventBus.addListener(BrazierItems::postRegistry);
-        modEventBus.addListener(BrazierContainers::postRegistry);
-        modEventBus.addListener(BrazierEntities::postRegistry);
-        modEventBus.addListener(BrazierSounds::postRegistry);
-    }
+		modEventBus.addListener(BrazierBlocks::postRegistry);
+		modEventBus.addListener(BrazierBlockEntities::postRegistry);
+		modEventBus.addGenericListener(Item.class, BrazierItems::registerItemBlocks);
+		modEventBus.addListener(BrazierItems::postRegistry);
+		modEventBus.addListener(BrazierContainers::postRegistry);
+		modEventBus.addListener(BrazierEntities::postRegistry);
+		modEventBus.addListener(BrazierSounds::postRegistry);
+	}
 
-    public void setupCommonStuff(FMLCommonSetupEvent event) {}
+	public void setupCommonStuff(FMLCommonSetupEvent event) {
+	}
 
-    public void setupServerStuff(FMLDedicatedServerSetupEvent event) {}
+	public void setupServerStuff(FMLDedicatedServerSetupEvent event) {
+	}
 }
